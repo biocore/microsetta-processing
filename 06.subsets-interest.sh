666356f96068a9b348b408a1dc96b703c4af464b
Daniel McDonald <mcdonadt@colorado.edu>
2020-12-02 17:52:41 -0800

    Tagging

--- 06.subsets-interest.sh
+++ 06.subsets-interest.sh
@@ -4,30 +4,30 @@ source ./util.sh
 
 # NOTE: the input (--metadata) is updated inplace
 python metadata_operations.py sample-status \
-    --metadata ${d}/raw.txt \
-    --original-table ${d}/raw.biom.qza \
-    --no-bloom-table ${d}/raw.biom.nobloom.qza \
-    --no-singletons-table ${d}/raw.biom.nobloom.minfeat.qza \
-    --min-count-table ${d}/raw.biom.nobloom.minfeat.mindepth.qza \
-    --only-inserted-table ${d}/raw.biom.nobloom.minfeat.mindepth.sepp.qza \
-    --rarefied-table ${d}/raw.biom.nobloom.minfeat.mindepth.sepp.even.qza
+    --metadata ${d}/$(tag).txt \
+    --original-table ${d}/$(tag).biom.qza \
+    --no-bloom-table ${d}/$(tag_nobloom).biom.qza \
+    --no-singletons-table ${d}/$(tag_minfeat).biom.qza \
+    --min-count-table ${d}/$(tag_mindepth).biom.qza \
+    --only-inserted-table ${d}/$(tag_treeoverlap).biom.qza \
+    --rarefied-table ${d}/$(tag_even).biom.qza
 
 python metadata_operations.py single-subject \
-    --table ${d}/raw.biom.nobloom.minfeat.mindepth.sepp.even.qza \
-    --metadata ${d}/raw.txt \
-    --output ${d}/raw.denotes-single-subject-sample.txt
+    --table ${d}/$(tag_even).biom.qza \
+    --metadata ${d}/$(tag).txt \
+    --output ${d}/$(tag).denotes-single-subject-sample.txt
 
 python metadata_operations.py columns-of-interest \
-    --metadata ${d}/raw.txt \
+    --metadata ${d}/$(tag).txt \
     --columns columns_of_interest.txt \
-    --output ${d}/raw.columns_of_interest.txt
+    --output ${d}/$(tag).columns_of_interest.txt
 
 for ar in ${d}/taxa/*.qza
 do
     name=$(basename ${ar} .qza)
     qiime feature-table filter-samples \
         --i-table ${ar} \
-        --m-metadata-file ${d}/raw.denotes-single-subject-sample.txt \
+        --m-metadata-file ${d}/$(tag).denotes-single-subject-sample.txt \
         --p-where "[single_subject_sample]='True'" \
         --o-filtered-table ${d}/taxa/${name}-single-subject-sample.qza
 done
@@ -37,7 +37,7 @@ do
     name=$(basename ${ar} .qza)
     qiime diversity filter-distance-matrix \
         --i-distance-matrix ${ar} \
-        --m-metadata-file ${d}/raw.denotes-single-subject-sample.txt \
+        --m-metadata-file ${d}/$(tag).denotes-single-subject-sample.txt \
         --p-where "[single_subject_sample]='True'" \
         --o-filtered-distance-matrix ${d}/beta/${name}-single-subject-sample.qza
 done
--- util.sh
+++ util.sh
@@ -12,16 +12,61 @@ then
     trim_length=None
     min_feature_count=100
     min_sample_depth=400000
+    rarefaction_replacement="with"
 else
     # NOTE: these need to be consistent!
     redbiom_ctx=Deblur-Illumina-16S-V4-100nt-fbc5b2
     trim_length=100
     min_feature_count=2
     min_sample_depth=1000
+    rarefaction_replacement="no-with"
 fi
 
+
+
+function tag_even () {
+    echo "$(tag_treeoverlap).even"
+}
+
+function tag_treeoverlap () {
+    basetag=$(tag_mindepth)
+    if [[ ${TMI_DATATYPE} == "16S" ]];
+    then
+        echo "${basetag}.sepp"
+    else
+        echo "${basetag}"
+    fi
+}
+
+function tag_mindepth () {
+    echo "$(tag_minfeat).mindepth"
+}
+
+function tag_minfeat () {
+    echo "$(tag_nobloom).minfeat"
+}
+
+function tag_nobloom () {
+    basetag=$(tag)
+    if [[ ${TMI_DATATYPE} == "WGS" ]];
+    then
+        echo "${basetag}"
+    else
+        echo "${basetag}.nobloom"
+    fi
+}
+
+function tag () {
+    echo "raw"
+}
+
 function base () {
-    echo "$(readlink ../current-${ENV_PACKAGE})"
+    if [ -z "${AG_DEBUG}" ]; then
+        echo "$(readlink ../current-${ENV_PACKAGE})"
+    else
+        mkdir -p ../current-debug
+        echo "../current-debug"
+    fi
 }
 
 if [ ! -d "$(base)" ]; then
