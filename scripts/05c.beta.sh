cdca8df92e6040273c1f0f75eb3a3c2a733c12a3
Daniel McDonald <mcdonadt@colorado.edu>
2021-03-12 17:41:10 -0800

    migrate k_nearest from pub api to processing

--- scripts/05c.beta.sh
+++ scripts/05c.beta.sh
@@ -12,6 +12,11 @@ qiime diversity beta-phylogenetic \
     --p-bypass-tips \
     --o-distance-matrix ${d}/beta/unweighted_unifrac.qza
 
+python k_neighbors.py \
+    --distance-matrix ${d}/beta/unweighted_unifrac.qza \
+    --output ${d}/beta/unweighted_unifrac_neighbors.qza \
+    --k 100
+
 if [[ ! -z "${TMI_WEIGHTED_UNIFRAC}" ]]; then
     qiime diversity beta-phylogenetic \
         --i-table ${d}/$(tag_even).biom.qza \
@@ -20,4 +25,9 @@ if [[ ! -z "${TMI_WEIGHTED_UNIFRAC}" ]]; then
         --p-metric weighted_normalized_unifrac \
         --p-bypass-tips \
         --o-distance-matrix ${d}/beta/weighted_normalized_unifrac.qza
+    
+    python k_neighbors.py \
+        --distance-matrix ${d}/beta/weighted_unifrac.qza \
+        --output ${d}/beta/weighted_unifrac_neighbors.qza \
+        --k 100
 fi
