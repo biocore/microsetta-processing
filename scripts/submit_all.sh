7920c0db5c0610d43a5f6f4b7f292a43ea7921bd
Daniel McDonald <mcdonadt@colorado.edu>
2021-03-14 17:40:38 -0700

    undelete some accidentally deleted lines

--- scripts/submit_all.sh
+++ scripts/submit_all.sh
@@ -3,6 +3,61 @@
 set -e
 set -x
 
+if [[ -z ${TMI_NAME} ]];
+then
+    echo "No name set!"
+    exit 1
+fi
+
+if [[ -z ${TMI_TITLE} ]];
+then
+    echo "No title set!"
+    exit 1
+fi
+
+if [[ -z ${TMI_DATATYPE} ]];
+then
+    echo "No data set!"
+    exit 1
+fi
+
+if [[ -z ${ENV_PACKAGE} ]];
+then
+    echo "No package set!"
+    exit 1
+fi
+
+if [[ -z ${STUDIES} ]];
+then
+    echo "No studies set!"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+datetag=$(date +"%Y-%m-%d.%H%M")
+logbase=../logs
+logdir=${logbase}/${datetag}-${TMI_NAME}-${TMI_DATATYPE}
+mkdir -p ${logdir}
+
+qsub_vars="-v ENV_PACKAGE=${ENV_PACKAGE}"
+qsub_vars=${qsub_vars},TMI_DATATYPE=${TMI_DATATYPE}
+qsub_vars=${qsub_vars},STUDIES=${STUDIES}
+qsub_vars=${qsub_vars},TMI_NAME=${TMI_NAME}
+qsub_vars=${qsub_vars},TMI_TITLE=$(echo ${TMI_TITLE} | tr " " ".")
+qsub_common="-o ${logdir} -e ${logdir} ${qsub_vars}"
+
+if [[ ! -z ${EMAIL} ]];
+then
+    qsub_common="${qsub_common} -M ${EMAIL} -m ae"
+fi
+
+qsub=qsub
+if [[ ! $(command -v qsub) ]]; 
+then
+    qsub=$(pwd)/qsub-capture.sh
+fi
+
+cwd=$(pwd)
 s01=$(echo "cd ${cwd}; bash 01.redbiom.sh" | ${qsub} -l nodes=1:ppn=1 -l mem=16g -l walltime=8:00:00 ${qsub_common} -N TMI01-${TMI_NAME})
 s02=$(echo "cd ${cwd}; bash 02.imports.sh" | ${qsub} -W depend=afterok:${s01} -l nodes=1:ppn=1 -l mem=16g -l walltime=2:00:00 ${qsub_common} -N TMI02-${TMI_NAME})
 s03=$(echo "cd ${cwd}; bash 03.filtering.sh" | ${qsub} -W depend=afterok:${s02} -l nodes=1:ppn=1 -l mem=16g -l walltime=2:00:00 ${qsub_common} -N TMI03-${TMI_NAME})
