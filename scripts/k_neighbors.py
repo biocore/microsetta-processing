f8e42abc146589d7ada8a52287fe205c3042e9de
Daniel McDonald <mcdonadt@colorado.edu>
2021-03-15 18:13:29 -0700

    Allow for masked nearest neighbors assessment

--- scripts/k_neighbors.py
+++ scripts/k_neighbors.py
@@ -2,13 +2,13 @@ import click
 import qiime2
 from skbio import DistanceMatrix
 import pandas as pd
+import pandas.testing as pdt
 import numpy as np
 import unittest
 
 
 class NeighborsTest(unittest.TestCase):
-    def test_closest_k_from_distance_matrix(self):
-        import pandas.testing as pdt
+    def test_get_neighbors(self):
         dm = DistanceMatrix(
             [
                 [0, 1, 2, 4, 5, 6],
@@ -31,8 +31,28 @@ class NeighborsTest(unittest.TestCase):
         obs = get_neighbors(dm, 2)
         pdt.assert_frame_equal(obs, exp)
 
+    def test_get_neighbors_mask(self):
+        dm = DistanceMatrix(
+            [
+                [0, 1, 2, 4, 5, 6],
+                [1, 0, 3, 4, 5, 6],
+                [2, 3, 0, 1, 1, 1],
+                [4, 4, 1, 0, 2, 9],
+                [5, 5, 1, 2, 0, 3],
+                [6, 6, 1, 9, 3, 0],
+            ], ids=['A.s1', 'A.s2', 'B.s3', 'B.s4', 'B.s5', 'A.s6'],
+        )
+        exp = pd.DataFrame([['B.s3', 'B.s4'],
+                            ['B.s3', 'B.s4'],
+                            ['B.s3', 'B.s5']],
+                           index=['A.s1', 'A.s2', 'A.s6'],
+                           columns=['k0', 'k1'])
+        exp.index.name = 'sample_id'
+        obs = get_neighbors(dm, 2, 'A')
+        pdt.assert_frame_equal(obs, exp)
 
-def get_neighbors(dm, k):
+
+def get_neighbors(dm, k, mask_study_id=None):
     """Constructs a dataframe containing only the closest k samples per sample
 
     Parameters
@@ -41,6 +61,9 @@ def get_neighbors(dm, k):
         The distance matrix to operate on
     k : int
         The number of samples to retain
+    mask_study_id : str, optional
+        If specified, only use the mask IDs as sources, and only consider
+        neighbors which do not match the mask.
 
     Returns
     -------
@@ -48,23 +71,29 @@ def get_neighbors(dm, k):
         Indexed by sample ID, the columns correspond to k0, k1, ... and
         the frame is valued by those similar sample IDs
     """
+    ids = dm.ids[:]
+    retain = lambda i: True
+
+    if mask_study_id is not None:
+        ids = [i for i in ids if i.split('.', 1)[0] == mask_study_id]
+        retain = lambda i: i.split('.', 1)[0] != mask_study_id
+
     results = []
-    for sample_id in dm.ids:
+    for sample_id in ids:
         sample_idx = dm.index(sample_id)
         distances = dm[sample_idx]
-        # has indices partitioned by distance, around the `kth` entry of the
-        # array
-        idx = np.argpartition(distances, kth=k)
-        # get the k + 1 closest samples (including this sample)
-        k_nearest_idx = idx[:k + 1]
-        # sort the k closest samples by their distance, so the closest are
-        k_distances = distances[k_nearest_idx]
-        # remove the sample itself
-        sorted_k_indices = np.argsort(k_distances)[1:]
-        k_nearest_idx = k_nearest_idx[sorted_k_indices]
-        nearest = [sample_id] + [dm.ids[idx]
-                                 for idx in k_nearest_idx]
-        results.append(nearest)
+        sorted_distances = np.argsort(distances)
+        sorted_ids = [dm.ids[i] for i in sorted_distances]
+
+        count = 0
+        nearest = []
+        for i in sorted_ids[1:]:
+            if retain(i):
+                nearest.append(i)
+                count += 1
+            if count >= k:
+                break
+        results.append([sample_id] + nearest)
 
     df = pd.DataFrame(results,
                       columns=['sample_id'] + ['k%d' % i for i in range(k)])
@@ -81,11 +110,18 @@ def cli():
               help='The Q2 distance matrix to operate on')
 @click.option('--output', type=click.Path(exists=False), required=True,
               help='Where to write the neighbors too')
+@click.option('--mask-study-id', type=str, required=False, default=None)
 @click.option('--k', type=int, required=True, help='The number of neighbors')
-def neighbors(distance_matrix, output, k):
+def neighbors(distance_matrix, output, mask_study_id, k):
     dm = qiime2.Artifact.load(distance_matrix).view(DistanceMatrix)
-    k = min(len(dm.ids), k + 1) - 1  # account for len(dm) == k
-    kn = get_neighbors(dm, k)
+
+    if mask_study_id is None:
+        k = min(len(dm.ids), k + 1) - 1  # account for len(dm) == k
+    else:
+        non_mask = [i for i in dm.ids if not i.startswith(mask_study_id)]
+        k = min(len(non_mask), k + 1) - 1
+
+    kn = get_neighbors(dm, k, mask_study_id)
     kn.to_csv(output, sep='\t', index=True, header=True)
 
 
