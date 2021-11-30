56b4d3d72c5d73487492f81168a5bbe0f7146b64
Daniel McDonald <mcdonadt@colorado.edu>
2021-11-30 09:55:19 -0800

    regeneration scripts

--- scripts/08.cleanup.sh
+++ scripts/08.cleanup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-python create-configuration.py \
+python cleanup.py create-configuration \
     --base=../results/current \
     --output=../results/current/api-config.json \
     --port 8082 \
     --copy-prefix /projects/tmi-public-results/${DATETAG} \
     --prefix /projects/tmi-public-results/${DATETAG} \
     --actually-copy
+
+python cleanup.py delete-unnecessary-files \
+    --base=../results/current 
